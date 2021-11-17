a8cbd37f1ad87b301bc376e3ea48b0748d8d4e5b
Kurikoko <zomahyon@gmail.com>
2021-11-17 14:19:39 +0900

    method="POST"テスト

--- src/main/java/com/example/demo/ShainController.java
+++ src/main/java/com/example/demo/ShainController.java
@@ -8,16 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ShainController {
 
-@RequestMapping("/input")
-public String index() {
-return "index.html";
-}
+	@RequestMapping("/input")
+	public String index() {
+		return "index.html";
+	}
 
-@RequestMapping("/output")
-public String result(@RequestParam("number") String number, Model model) {
-String name = "コントローラー太郎";
-model.addAttribute("number", number);
-model.addAttribute("name", name);
-return "output.html";
-}
+	@RequestMapping("/output")
+	public String result(@RequestParam("number") String number, Model model) {
+		String name = "コントローラー太郎";
+		model.addAttribute("number", number);
+		model.addAttribute("name", name);
+		return "output.html";
+	}
 }
